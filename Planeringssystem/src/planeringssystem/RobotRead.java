e31511a0177c957347f8f09e1004c4d77d3e6c5f
Anna Danielsson <annda430@student.liu.se>
2018-03-06 14:04:45 +0100

    La till klassen RobotRead

---
+++ Planeringssystem/src/planeringssystem/RobotRead.java
@@ -0,0 +1,34 @@
+package planeringssystem;
+
+import java.util.Random;
+
+public class RobotRead implements Runnable {
+
+    private int sleepTime;
+    private static Random generator = new Random(); // Vet inte om vi kommer behöva denna?
+    private GUI gui;
+    private DataStore ds;
+
+    public RobotRead(DataStore ds, GUI gui) {
+        this.gui = gui;
+        this.ds = ds;
+        sleepTime = generator.nextInt(20000);
+    }
+
+    @Override
+    public void run() {
+        try {
+            gui.appendErrorMessage("RobotRead kommer att köra i " + sleepTime + " millisekunder.");
+
+            int i = 1;
+            while (i <= 20) {
+                Thread.sleep(sleepTime / 20);
+                gui.appendErrorMessage("Jag är tråd RobotRead för " + i + ":e gången.");
+                i++;
+            }
+        } catch (Exception e) {
+        }
+
+        gui.appendErrorMessage("Robotread är nu klar");
+    }
+}
