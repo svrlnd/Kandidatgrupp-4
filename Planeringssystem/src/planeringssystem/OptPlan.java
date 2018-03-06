25700e223e00d81fc9d34bd767a6d81507b82747
Simon Siverland <simsi575@student.liu.se>
2018-03-06 15:03:13 +0100

    Skapat classen OptPlan

---
+++ Planeringssystem/src/planeringssystem/OptPlan.java
@@ -0,0 +1,56 @@
+package planeringssystem;
+
+import java.util.*;
+
+
+public class OptPlan {
+    
+    private List<Vertex> nodes;
+    private List<Edge> edges;
+    private DataStore ds;
+    
+    public OptPlan (DataStore ds){
+        this.ds = ds;
+    }
+    
+    public void createPlan(){
+        nodes = new ArrayList<Vertex>();
+        edges = new ArrayList<Edge>();
+        
+        //Här hade vi scanner för att läsa in var vi ville börja och sluta
+        //Men det tog jag väck, för det ska vi ju läsa av hos AGV sen /S
+        
+        int start_node = 1;
+        int dest_node = 50;
+        
+        //Set up network
+        for (int i = 0; i < ds.nodes; i++){
+            Vertex location = new Vertex("" + (i+1), "Nod #" + (i+1));
+            nodes.add(location);
+        }
+        for (int i = 0; i < ds.arcs; i++){
+            Edge lane = new Edge ("" +  (i+1), nodes.get(ds.arcStart[i]-1),
+            nodes.get(ds.arcEnd[i] - 1), 1); //Last argument is arccost
+            edges.add(lane);
+        }
+        
+        Graph graph = new Graph(nodes,edges);
+        DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
+        
+        //Beräkna kortaste rutt
+        dijkstra.execute(nodes.get(start_node));
+        LinkedList<Vertex> path = dijkstra.getPath(nodes.get(dest_node));
+        
+        //Hämta kortast rutt
+        for (int i = 0; i < path.size()-1; i++){
+            for (int j = 0; j < ds.arcs; j++){
+                if (ds.arcStart[j] == Integer.parseInt(path.get(i+1).getId())
+                        && ds.arcEnd[j] == Integer.parseInt(path.get(i+1).getId())){
+                    
+                    System.out.println("Arc: " + j);
+                    ds.arcColor[j]=1;
+                }
+            }
+        }  
+    }
+}
