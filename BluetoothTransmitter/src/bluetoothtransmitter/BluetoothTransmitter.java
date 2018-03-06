d2405572f9dcc87727d07ea172bf53160cd5eab6
Svrlnd <gsedvallson@gmail.com>
2018-03-06 12:13:21 +0100

    La in bluetooth-filer samt http-anrop som separata projekt i repot. Förmodligen kommer dessa behöva läggas i samma projekt senare.

---
+++ BluetoothTransmitter/src/bluetoothtransmitter/BluetoothTransmitter.java
@@ -0,0 +1,29 @@
+package bluetoothtransmitter;
+
+import java.io.*;
+import javax.microedition.io.*;
+import javax.bluetooth.*;
+
+public class BluetoothTransmitter {
+
+    public static void
+            main(String args[]) {
+        try {
+            StreamConnection anslutning = (StreamConnection) Connector.open(
+                    "btspp://001060D1CDC2:5"
+            );
+            PrintStream bluetooth_ut
+                    = new PrintStream(anslutning.openOutputStream());
+
+            bluetooth_ut.println(
+                    "Test från grupp 4"
+            );
+            Thread.sleep(
+                    500
+            );
+            anslutning.close();
+        } catch (Exception e) {
+            System.out.print(e.toString());
+        }
+    }
+}
