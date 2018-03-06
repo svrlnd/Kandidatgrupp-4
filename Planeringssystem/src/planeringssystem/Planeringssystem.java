577738b8110f8f03d3178c648073e79ba92e833f
Anna Danielsson <annda430@student.liu.se>
2018-03-06 12:20:49 +0100

    tog bort onödiga kommentarer

--- Planeringssystem/src/planeringssystem/Planeringssystem.java
+++ Planeringssystem/src/planeringssystem/Planeringssystem.java
@@ -19,7 +19,7 @@ public class Planeringssystem {
          * Initialize the DataStore call where all "global" data will be stored
          */
         ds = new DataStore();
-        //testing testing
+        
 
         /*
          * Reads in the file with the map. For now it is street.txt. Don´t know where the file will be stored.
@@ -40,7 +40,7 @@ public class Planeringssystem {
     public static void main(String[] args) {
         
         Planeringssystem x = new Planeringssystem();
-        // annda430
+        
     }
 
 }
