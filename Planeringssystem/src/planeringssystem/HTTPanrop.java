31ebd7e496621a2f2adaa2be4220f5d6f38a563e
Svrlnd <gsedvallson@gmail.com>
2018-03-22 16:20:53 +0100

    Nu ska det funka

--- Planeringssystem/src/planeringssystem/HTTPanrop.java
+++ Planeringssystem/src/planeringssystem/HTTPanrop.java
@@ -1,22 +1,25 @@
-package httpanrop;
+package planeringssystem;
 
-/**
- *
- * @author itn
- */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HTTPanrop {
+    
+    private int passengers;
+    
+    public int getPassengers () {
+        return passengers;
+    }
 
     public static void
             main(String[] args) {
         try {
+                    
             HTTPanrop http = new HTTPanrop();
             String url
-                    = "http://tnk111.n7.se/tauppdrag.php?plats=A&id=1&passagerare=4&grupp=4";
+                    = //"http://tnk111.n7.se/tauppdrag.php?plats=A&id="+id+",2&passagerare=1&grupp=1";  //"http://tnk111.n7.se/tauppdrag.php?plats=A&id=1&passagerare=8&grupp=4" , "http://tnk111.n7.se/aterstall.php?scenario=1"
             URL urlobjekt = new URL(url);
             HttpURLConnection anslutning = (HttpURLConnection) urlobjekt.openConnection();
             System.out.println(
