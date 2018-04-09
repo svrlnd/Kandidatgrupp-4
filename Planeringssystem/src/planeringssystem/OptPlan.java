b1895751d7187a8a7f6e87a6e63c2699ccf1d0e6
annda30 <anna.danielsson95@hotmail.se>
2018-04-09 14:14:29 +0200

    La till början på hur ett meddelande till AGV kan se ut i planeringssystem

--- Planeringssystem/src/planeringssystem/OptPlan.java
+++ Planeringssystem/src/planeringssystem/OptPlan.java
@@ -19,7 +19,7 @@ public class OptPlan {
         //Här hade vi scanner för att läsa in var vi ville börja och sluta
         //Men det tog jag väck, för det ska vi ju läsa av hos AGV sen /S
         int start_node = 70; // Här ska vi istället ta AGVs nuvarande position, från BT-klassen
-        int dest_node = 24; // Här ska vi istället ta in platser från HTTP-server
+        int dest_node = 3; // Här ska vi istället ta in platser från HTTP-server
 
         int start_arc = 1; //här vill vi läsa in vilket nod vi är på?
         int dest_arc = 17;
