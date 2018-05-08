6124235507e2c8341c187632e09ffd83e22ccd0e
annda30 <anna.danielsson95@hotmail.se>
2018-05-08 14:44:32 +0200

    la till att en ny rutt startas om tauppdrag beviljas, samt gjort om ds.uppdrag till en array

--- Planeringssystem_v2/src/planeringssystem_v2/GroupRead.java
+++ Planeringssystem_v2/src/planeringssystem_v2/GroupRead.java
@@ -219,7 +219,7 @@ public class GroupRead implements Runnable {
 //            }
 //        }
 
-        msg = ds.valdPlats + "!" + ds.distanceCP + "!" + ds.uppdrag;
+        msg = ds.valdPlats + "!" + ds.distanceCP + "!" + ds.uppdrag[0] + "," + ds.uppdrag[1];
 
         hg.putmessage(msg);
 
